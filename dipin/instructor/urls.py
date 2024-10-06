c5532adf1736d508ec627706ed95d22003c37a3f
Dipin D <dipindawadi@gmail.com>
2024-10-06 18:39:31 -0500

    Added content management; quiz and lecture functionalities are now working.

--- dipin/instructor/urls.py
+++ dipin/instructor/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('', views.class_shell_list, name='class_shell_list'),
     path('create/', views.create_class_shell, name='create_class_shell'),
     path('course/<int:class_shell_id>/', views.go_to_course, name='go_to_course'),
+    path('quizlist/<int:class_shell_id>/', views.quizlist, name='quizlist'),  # Add class_shell_id here
+    path('create_quiz/<int:class_shell_id>/', views.create_quiz, name='create_quiz'),
+    path('question/<int:class_shell_id>/<int:quiz_id>/', views.go_to_quiz, name='go_to_quiz'),
+    path('class-shell/<int:class_shell_id>/assignments/', views.assignment, name='assignment'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
