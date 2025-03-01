676bbb184bd6d8b19456b2a0ef55159cffe9b59e
Dipin-D <dipindawadi@gmail.com>
2025-03-01 12:13:22 -0600

    added timer

--- dipin/dipin/settings.py
+++ dipin/dipin/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
+TIME_ZONE = 'America/Chicago'  
+USE_TZ = True  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
