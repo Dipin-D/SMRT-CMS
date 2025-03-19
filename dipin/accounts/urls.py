4b2d9bea0c6e67a675dbf4e0c3745b7ba07211aa
Dipin-D <dipindawadi@gmail.com>
2025-03-18 19:51:32 -0500

    phase-3 design

--- dipin/accounts/urls.py
+++ dipin/accounts/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),    
+    path("guest-login/", views.guest_login, name="guest_login"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
