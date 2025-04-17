38d2a0ce1390197506ce0825c4cdd4cc5b55ae3a
Dipin-D <dipindawadi@gmail.com>
2025-04-17 13:24:51 -0500

    timer bug fixes

---
+++ .history/dipin/student/urls_20250417081449.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import course_list, go_to_course_student,attempt_quiz, attempt_exercise,while_quiz, while_exercise
+
+app_name = 'student'
+
+urlpatterns = [
+    path('courses/', course_list, name='course_list'),
+    path('courses/<int:class_shell_id>/', go_to_course_student.as_view(), name='go_to_course'),
+    path('course/<int:class_shell_id>/lectures/<int:lecture_id>/', go_to_course_student.as_view(), name='view_lecture'),
+    path('quizzes/<int:class_shell_id>/', go_to_course_student.as_view(),name='quiz_list'),
+    path('attempt_quiz/<int:class_shell_id>/<int:quiz_id>/', attempt_quiz, name='attempt_quiz'),
+    path('while_quiz/<int:class_shell_id>/<int:quiz_id>/', while_quiz, name='while_quiz'),
+    path('exercise/<int:class_shell_id>/<int:exercise_id>/', attempt_exercise, name='attempt_exercise'),
+    path('exercise/<int:class_shell_id>/<int:exercise_id>/while/', while_exercise, name='while_exercise'),
+    path('courses/<int:class_shell_id>/assignments/<int:assignment_id>/', go_to_course_student.as_view(), name='view_assignment'),
+    path('courses/<int:class_shell_id>/exercises/<int:exercise_id>/', go_to_course_student.as_view(), name='view_exercise'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
