354362644ab5df0722d33292c5be2e20def54431
Dipin-D <dipindawadi@gmail.com>
2025-04-24 11:23:11 -0500

    testing admin page

--- dipin/dipin/urls.py
+++ dipin/dipin/urls.py
@@ -5,10 +5,15 @@ from django.conf import settings
 from accounts.views import front_page
 from django.shortcuts import redirect
 from django.contrib.auth.urls import views as auth_views
+from django.http import HttpResponseNotFound, HttpResponseForbidden
 
 
-# def redirect_to_home(request, exception=None):
-#     return redirect('/')
+def redirect_to_home(request, exception=None):
+    if request.path.startswith('/admin') or request.path.startswith('/accounts/login'):
+        if isinstance(exception, PermissionError):
+            return HttpResponseForbidden("Permission denied.")
+        return HttpResponseNotFound("Page not found.")
+    return redirect('/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,5 +26,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# handler403 = redirect_to_home
-# handler404 = redirect_to_home
+handler403 = redirect_to_home
+handler404 = redirect_to_home
